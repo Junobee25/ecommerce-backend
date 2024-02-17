32e81df0c25485a82028e36985eb97519529c5d7
Junobee25 <wnsdhqo@naver.com>
2024-02-17 21:25:30 +0900

    #4 - 동시성 문제 테스트

---
+++ product-service/src/test/java/com/hanghae/productservice/service/ProductServiceTest.java
@@ -0,0 +1,50 @@
+package com.hanghae.productservice.service;
+
+import com.hanghae.productservice.domain.constant.ProductType;
+import com.hanghae.productservice.domain.entity.Product;
+import com.hanghae.productservice.domain.entity.Stock;
+import com.hanghae.productservice.domain.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductServiceTest {
+
+
+    @Autowired private ProductRepository productRepository;
+    @Autowired private ProductLockFacade productLockFacade;
+    @Autowired private ProductService productService;
+
+    @Test
+    void 동시에_100명이_신발을_구매한다() throws InterruptedException {
+        Long productId = productRepository.save(Product.of("나이키", 36_000, "예쁜 나이키 신발", ProductType.NOT_RESERVATION, Stock.of(100L)))
+                .getId();
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        CountDownLatch countDownLatch = new CountDownLatch(100);
+
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                try {
+                    productLockFacade.purchase(productId, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        Product actual = productRepository.findById(productId)
+                .orElseThrow();
+
+        assertThat(actual.getStock().getRemain()).isZero();
+    }
+
+}
