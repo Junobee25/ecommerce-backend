56f65fc847fc15e735118aa5a557c8d58af452af
Junobee25 <wnsdhqo@naver.com>
2024-02-23 11:46:56 +0900

    #9 - 결제 서비스 리팩토링 및 잔액 부족 취소 로직 적용
    결제 진입, 취소, 결제 진행 프로세스에 필요한 중복되는 로직을
    메서드로 추출했고 결제 진행 중 잔액 부족 이탈에 대한 로직을 구현했다.

--- payment-service/src/main/java/com/hanghae/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/hanghae/paymentservice/service/PaymentService.java
@@ -25,44 +25,41 @@ public class PaymentService {
     @Transactional
     public void entryPayment(HttpHeaders headers) {
         //TODO: 주문 정보 입력 후 결제 시작 버튼 -> 재고 수량 감소
-        String userEmail = userServiceClient.getUserEmail(headers);
-        Long userId = userServiceClient.getUserId(userEmail);
+        processPayment(headers);
+    }
 
-        List<OrdersInfoDto> orders = ordersServiceClient.getOrdersInfo(userId);
-        List<StockHistoryDto> stockHistoryList = getStockHistory(orders);
-
-        stockHistoryList.forEach(stockServiceClient::decreaseStock);
-
+    @Transactional
+    public void payment(HttpHeaders headers) {
+        //TODO: 20% 이탈 고려하여 Random으로 Redis DB 내 유저 주문결제 정보 삭제
+        cancel(headers);
     }
 
     @Transactional
     public void cancel(HttpHeaders headers) {
         //TODO: 결제 정보 입력(카드사, 계좌 번호, etc..)창 결제 취소 버튼 사용자 의도적 이탈 20%
         if (calculateFailure()) {
-            String userEmail = userServiceClient.getUserEmail(headers);
-            Long userId = userServiceClient.getUserId(userEmail);
-
-            List<OrdersInfoDto> orders = ordersServiceClient.getOrdersInfo(userId);
-            List<StockHistoryDto> stockHistoryList = getStockHistory(orders);
-
-            stockHistoryList.forEach(stockServiceClient::increaseStock);
+            processPayment(headers);
         }
     }
 
-    @Transactional
-    public void payment(@RequestHeader HttpHeaders headers) {
-        //TODO: 20% 이탈 고려하여 Random으로 Redis DB 내 유저 주문결제 정보 삭제
+    private void processPayment(HttpHeaders headers) {
+        String userEmail = userServiceClient.getUserEmail(headers);
+        Long userId = userServiceClient.getUserId(userEmail);
 
+        List<OrdersInfoDto> orders = ordersServiceClient.getOrdersInfo(userId);
+        List<StockHistoryDto> stockHistoryList = getStockHistory(orders);
+
+        stockHistoryList.forEach(stockServiceClient::increaseStock);
+    }
+
+    private Boolean calculateFailure() {
+        int randomValue = new Random().nextInt(100);
+        return randomValue < 20;
     }
 
     private List<StockHistoryDto> getStockHistory(List<OrdersInfoDto> paymentInfo) {
         return paymentInfo.stream()
                 .map(info -> StockHistoryDto.of(info.productId(), info.quantity()))
                 .toList();
-    }
-
-    private Boolean calculateFailure() {
-        int randomValue = new Random().nextInt(100);
-        return randomValue < 20;
     }
 }
