d93adc2f5bda6ab5531d330373218be54d38f502
Junobee25 <wnsdhqo@naver.com>
2024-02-25 00:06:01 +0900

    #9 - 결제 완료, 취소 로직 수정
    결제 완료, 결제 진행 상태를 구분하기 위해 OrderStatus 중 `PROGRESS` 필드를 추가 하였었다. 이를 적용한 로직이고 FeignClient로 연결 해주었다.

--- order-service/src/main/java/com/hanghae/orderservice/external/service/ExternalService.java
+++ order-service/src/main/java/com/hanghae/orderservice/external/service/ExternalService.java
@@ -1,5 +1,7 @@
 package com.hanghae.orderservice.external.service;
 
+import com.hanghae.orderservice.domain.constant.OrderStatus;
+import com.hanghae.orderservice.domain.entity.Orders;
 import com.hanghae.orderservice.domain.repository.OrdersRepository;
 import com.hanghae.orderservice.external.controller.dto.OrdersWithPaymentAdapterDto;
 import lombok.RequiredArgsConstructor;
@@ -23,5 +25,21 @@ public class ExternalService {
                         order.getTotalPrice()))
                 .collect(Collectors.toList());
 
+    }
+
+    public void completeOrders(Long userId) {
+        ordersRepository.findByUserId(userId)
+                .forEach(order -> {
+                    order.setOrderStatus(OrderStatus.COMPLETE);
+                    ordersRepository.save(order);
+                });
+    }
+
+    public void cancelOrders(Long userId) {
+        ordersRepository.findByUserId(userId)
+                .forEach(order -> {
+                    order.setOrderStatus(OrderStatus.CANCEL);
+                    ordersRepository.save(order);
+                });
     }
 }
